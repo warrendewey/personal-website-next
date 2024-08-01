71a91080220d655a83c32e9b8c375d2acd6af87f
warrendewey <warrendewey8@gmail.com>
2024-08-01 15:30:47 -0500

    Adding link to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Daddy's home!</Button>
+      <Button>
+        <Link href="/projects/projectId">Daddy's home!</Link>
+      </Button>
     </main>
   );
 }
--- src/components/ui/nav-bar.tsx
+++ src/components/ui/nav-bar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <nav className="w-full border-0 py-4 lg:px-24 px -10 bg-gray-950">
       <h1 className="text-3xl text-white">
-        <Link href="/dashboard">Warren's Portfolio</Link>
+        <Link href="/">Warren's Portfolio</Link>
       </h1>
     </nav>
   );
