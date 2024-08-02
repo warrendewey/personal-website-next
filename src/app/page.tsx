99dfe996963b1e6b03b7fbe9481ee0be385a7897
warrendewey <warrendewey8@gmail.com>
2024-08-02 00:56:00 -0500

    My badges aren't working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
+import { Badge, Link } from "lucide-react";
 
 export default function Home() {
+  const projects: Array<string> = [
+    "Jewelry",
+    "Photography",
+    "Apps",
+    "Research",
+    "Jobs",
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        <Link href="/projects/projectId">Daddy's home!</Link>
-      </Button>
-    </main>
+    <div>
+      {projects.map((project, idx) => (
+        <Badge>
+          key={"${project}-${idx}"}
+          variant={"outline"}
+          className="border-orange-800 text-gray-900 text-lg mx-2 my-1
+          hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in
+          duration-200"
+          {project}
+        </Badge>
+      ))}
+    </div>
   );
 }
